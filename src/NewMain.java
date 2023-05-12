66aa8ea651976a08e0ec1bf177f743283b16a859
Alexander <alnerius88@yandex.ru>
2023-05-12 14:22:57 +0300

    Initial

--- src/NewMain.java
+++ src/NewMain.java
@@ -1,7 +1,8 @@
 
 public class NewMain {
     public static void main (String [] args ) {
-        Book phoenix;
+        Book phoenix = new Book ("The Phoenix on the Sword", "Robert Howard", 1932 );
         phoenix.setPublicationYear(1933);
+        System.out.println("phoenix = " + phoenix.getPublicationYear());
     }
 }
