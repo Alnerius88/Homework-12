01b39363c2f66b6152adcc80ef003a1d87752dca
Alexander <alnerius88@yandex.ru>
2023-05-12 13:57:50 +0300

    Initial

--- src/NewMain.java
+++ src/NewMain.java
@@ -3,6 +3,5 @@ public class NewMain {
     public static void main (String [] args ) {
         Book phoenix;
         phoenix.setPublicationYear(1933);
-        System.out.println("phoenix = " + phoenix);
     }
 }
