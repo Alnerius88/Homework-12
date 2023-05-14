d9141cfbbd4b0abe4c06f3fbded36a9045b0b023
Alexander <alnerius88@yandex.ru>
2023-05-14 09:00:00 +0300

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,4 @@
 public class Main {
     public static void main (String[] args) {
-        Book phoenix = new Book ("The Phoenix on the Sword", "Robert Howard", 1932 );
-        Book scarletStudy = new Book("A Study in Scarlet","Arthur Conan Doyle", 1887);
-        Author rHoward = new Author("Robert", "Howard");
-        Author artConDoyle = new Author("Arthur", "Conan Doyle");
-        System.out.println(phoenix.getBookName() + ", " + phoenix.getAuthor() +", "+ phoenix.getPublicationYear());
-        System.out.println(scarletStudy.getBookName() + ", " + scarletStudy.getAuthor() + "," + scarletStudy.publicationYear);
-        System.out.println(rHoward.getFirstName() +" "+ rHoward.getLastName());
-        System.out.println(artConDoyle.getFirstName() + " " + artConDoyle.getLastName());
-
-    }
+        System.out.println("Hello World");}
 }
